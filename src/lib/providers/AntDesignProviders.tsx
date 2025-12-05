b1f4841058c2a1177759809eb6c1f7d9d5469f96
shpakovad <shpakovad@gmail.com>
2025-12-05 14:34:32 +0300

    Added common font family

--- src/lib/providers/AntDesignProviders.tsx
+++ src/lib/providers/AntDesignProviders.tsx
@@ -1,10 +1,10 @@
 'use client';
 
 import '@ant-design/v5-patch-for-react-19';
+import { ReactNode } from 'react';
 import { ConfigProvider } from 'antd';
 import { StyleProvider } from '@ant-design/cssinjs';
 import { AntdRegistry } from '@ant-design/nextjs-registry';
-import { ReactNode } from 'react';
 
 interface Props {
   children: ReactNode;
@@ -14,12 +14,13 @@ export function AntDesignProviders({ children }: Props) {
     <AntdRegistry>
       <StyleProvider layer>
         <ConfigProvider
-            theme={{
-                token: {
-                    fontFamily: "'Roboto', sans-serif",
-                },
-            }}
-        >{children}
+          theme={{
+            token: {
+              fontFamily: "'Roboto', sans-serif",
+            },
+          }}
+        >
+          {children}
         </ConfigProvider>
       </StyleProvider>
     </AntdRegistry>
